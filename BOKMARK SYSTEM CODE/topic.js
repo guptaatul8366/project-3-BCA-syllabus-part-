7be06e55fdfc0afb60c1e555f5b34ecc23af7df1
guptaatul8366 <gupta.atul.8366@gmail.com>
2024-08-24 22:56:10 +0530

    Add files via upload

---
+++ BOKMARK SYSTEM CODE/topic.js
@@ -0,0 +1,20 @@
+let contentBoxOne=document.querySelector('.one')
+contentBoxOne.addEventListener("click",()=>{
+    window.location.href="dbms.html";
+    
+})
+let contentBoxTwo=document.querySelector('.two')
+contentBoxTwo.addEventListener("click",()=>{
+    window.location.href="software.html";
+    
+})
+let contentBoxThree=document.querySelector('.three')
+contentBoxThree.addEventListener("click",()=>{
+    window.location.href="web.html";
+    
+})
+let contentBoxFour=document.querySelector('.four')
+contentBoxFour.addEventListener("click",()=>{
+    window.location.href="os.html";
+    
+})
